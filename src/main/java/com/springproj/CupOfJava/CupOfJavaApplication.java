2b129c5b929b37cb106d3af77b49338781ad3675
JasonAg <jason.aguilera.s@gmail.com>
2021-02-01 17:56:12 -0500

    first project commit

---
+++ src/main/java/com/springproj/CupOfJava/CupOfJavaApplication.java
@@ -0,0 +1,13 @@
+package com.springproj.CupOfJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CupOfJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CupOfJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springproj/CupOfJava/CupOfJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproj.CupOfJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CupOfJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
